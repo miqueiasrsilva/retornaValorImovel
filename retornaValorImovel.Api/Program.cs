40b7b09068062276e517198a92a98661a5adb8ca
miqueiasrsilva <mi.ike@hotmail.com>
2020-12-21 12:38:41 -0300

    Http Heroku

--- retornaValorImovel.Api/Program.cs
+++ retornaValorImovel.Api/Program.cs
@@ -21,6 +21,7 @@ namespace retornaValorImovel
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
                 });
     }
 }
